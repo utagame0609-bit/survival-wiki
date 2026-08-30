c90ab6a9fb5b7fb7c664e6a9a34b5459bc9c449f
utagame0609-bit <uta.game.0609@gmail.com>
2026-08-30 21:55:56 +0900

    style: load SCP dossier article skin

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './hudBrackets.css';
+import './wikiScpDossier.css';
 import { initializeUserSoundSettings } from './lib/userSoundSettingsBootstrap';
 
 void initializeUserSoundSettings();
