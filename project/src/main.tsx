424f5f6a8655239e1e4cc2cd07330a4debd651a6
utagame0609-bit <uta.game.0609@gmail.com>
2026-08-30 15:30:55 +0900

    style: load shared HUD bracket accents

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './hudBrackets.css';
 import { initializeUserSoundSettings } from './lib/userSoundSettingsBootstrap';
 
 void initializeUserSoundSettings();
