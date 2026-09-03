ad37a9ccec267de4381c3bf3ba800d38111b20d5
utagame0609-bit <uta.game.0609@gmail.com>
2026-09-04 07:36:07 +0900

    Initialize persisted app theme before render

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -5,7 +5,9 @@ import './index.css';
 import './styles/hudBrackets.css';
 import './styles/wikiScpDossier.css';
 import { initializeUserSoundSettings } from './lib/userSoundSettingsBootstrap';
+import { initializeAppTheme } from './lib/theme';
 
+initializeAppTheme();
 void initializeUserSoundSettings();
 
 createRoot(document.getElementById('root')!).render(
